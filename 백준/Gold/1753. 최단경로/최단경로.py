549c12b1749a0a2292547bbeae93de9a04f5bef1
Seona <105192908+seona-moon@users.noreply.github.com>
2024-02-20 14:55:59 +0900

    [Gold IV] Title: 최단경로, Time: 632 ms, Memory: 68552 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,48 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+INF = sys.maxsize
+V, E = map(int, input().split())
+#시작점 K
+K = int(input())
+#가중치 테이블 dp (distance)
+dp = [INF]*(V+1)
+heap = []
+adj = [[] for _ in range(V + 1)]
+
+def Dijkstra(start):
+    #가중치 테이블에서 시작 정점에 해당하는 가중치는 0으로 초기화
+    dp[start] = 0
+    heapq.heappush(heap,(0, start))
+
+    #힙에 원소가 없을 때 까지 반복.
+    while heap:
+        wei, now = heapq.heappop(heap)
+
+        #현재 테이블과 비교하여 불필요한(더 가중치가 큰) 튜플이면 무시.
+        if dp[now] < wei:
+            continue
+        
+        # 연결 노드 탐색
+        for w, next_node in adj[now]:
+            #현재 정점 까지의 가중치 wei + 현재 정점에서 다음 정점(next_node)까지의 가중치 W
+            # = 다음 노드까지의 가중치(next_wei)
+            next_wei = w + wei
+            #다음 노드까지의 가중치(next_wei)가 현재 기록된 값 보다 작으면 조건 성립.
+            if next_wei < dp[next_node]:
+                #계산했던 next_wei를 가중치 테이블에 업데이트. (최솟값을 업데이트)
+                dp[next_node] = next_wei
+                #다음 점 까지의 가증치와 다음 점에 대한 정보를 튜플로 묶어 최소 힙에 삽입.
+                heapq.heappush(heap,(next_wei,next_node))
+
+#초기화
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    #(가중치, 목적지 노드) 형태로 저장 -> 최소힙으로 가중치가 작은 것부터 빼내려고
+    adj[u].append((w, v))
+
+Dijkstra(K)
+
+for i in range(1,V+1):
+    print("INF" if dp[i] == INF else dp[i])
