0e1d839c3a03141493849794e12718936d44f02a
Seona <105192908+seona-moon@users.noreply.github.com>
2024-02-23 16:53:06 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,8 +1,5 @@
-SELECT DISTINCT a.ID, a.EMAIL, a.FIRST_NAME, a.LAST_NAME
-FROM DEVELOPERS a, SKILLCODES b
-WHERE 
-#b'1000' & b'1111', // 첫 번째 비트만이 둘 다 1이므로, 연산 결과는 b'1000'이 됨.
-#b'1000' | b'1111', // 모든 비트에 하나라도 1이 포함되어 있으므로, 연산 결과는 b'1111'이 됨.
-    (a.SKILL_CODE & b.CODE) > 0 # 코드 중 연관되는 값이 하나라도 있다면?
-    AND b.NAME IN ("Python", "C#") # 그리고 그 값이 파이썬이나 C라면?
-ORDER BY a.ID;
+SELECT DISTINCT D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
+FROM DEVELOPERS D, SKILLCODES S
+WHERE D.SKILL_CODE & S.CODE # 코드 중 일치하는게 있는지 확인 (이진수 교집합 사용)
+AND S.NAME IN ('Python', 'C#')
+ORDER BY D.ID
