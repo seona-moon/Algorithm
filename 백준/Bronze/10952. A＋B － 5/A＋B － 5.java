953e0999eaee44d5e89bee028a73a54ac05bbb42
Seona <105192908+seona-moon@users.noreply.github.com>
2024-04-03 17:29:44 +0900

    [Bronze V] Title: A+B - 5, Time: 120 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while (true) {
+            line = br.readLine();
+            if (line.charAt(0) == '0')
+                break;
+            sb.append(line.charAt(0) - '0' + line.charAt(2) - '0');
+            sb.append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
