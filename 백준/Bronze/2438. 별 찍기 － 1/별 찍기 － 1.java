80079522a832737ed77b6697de2395bc7f213ca5
Seona <105192908+seona-moon@users.noreply.github.com>
2024-04-04 19:12:45 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            sb.append("*".repeat(Math.max(0, i)));
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
