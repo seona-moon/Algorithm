1f2adc14b9b9e6b2db4b1874d3f860906170f2e6
Seona <105192908+seona-moon@users.noreply.github.com>
2024-09-09 18:18:36 +0900

    [Gold III] Title: 나머지 합, Time: 348 ms, Memory: 257156 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -5,23 +5,23 @@ n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # 구간 합 구하기
-dp = [0] * (n + 1)
-for i in range(n):
-    dp[i + 1] = arr[i] + dp[i]
+dp = [0] * n
+dp[0] = arr[0]
+for i in range(1, n):
+    dp[i] = arr[i] + dp[i - 1]
 
-# 구간 합 나머지 업데이트
+# 구간 합 나머지 업데이트 (동시에 나머지 배열 초기화도 같이)
 cnt = 0
-for i in range(1, n + 1):
+rem = [0] * m  # m으로 나눈 나머지는 0~m-1 밖에 없음.
+
+for i in range(n):
     dp[i] %= m
     if dp[i] == 0:
         cnt += 1  # 나머지가 0인 경우, 길이가 1일때 무조건 성립
+    rem[dp[i]] += 1  # 해당 나머지를 가진 값 +1
 
 # 나머지가 같은 조합의 수를 계산하는 문제로 변형
-for i in range(m):  # m으로 나눈 나머지는 0~m-1 밖에 없음.
-    num = 0  # 나머지가 같은 값의 개수
-    for j in range(1, n + 1):
-        if dp[j] == i:
-            num += 1  # 나머지가 같은 값들을 다 더한다.
-    if num >= 2:
-        cnt += num * (num - 1) / 2  # nC2를 계산
+for i in range(m):
+    if rem[i] >= 2:
+        cnt += rem[i] * (rem[i] - 1) / 2  # nC2를 계산
 print(int(cnt))
