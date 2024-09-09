d578cd2f291c0a73cfa39f4a8f1bc3d7afc7eb45
Seona <105192908+seona-moon@users.noreply.github.com>
2024-09-09 16:57:10 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 236 ms, Memory: 41116 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,15 +1,22 @@
 import sys
+
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-array = list(map(int, input().split()))
-dp = [0]*(n+1)
+arr = list(map(int, input().split()))
 
-dp[0] = 0
-dp[1] = array[0]
-for i in range(2,n+1):
-    dp[i] = dp[i-1]+array[i-1]
+# Prefix Sum 배열 생성
+prefix_sum = [0] * n
+prefix_sum[0] = arr[0]
 
+# 구간 합 구하기
+for i in range(1, n):
+    prefix_sum[i] = prefix_sum[i - 1] + arr[i]
+
+# 테스트 케이스 진행
 for _ in range(m):
-    s, e = map(int,input().split())
-    print(dp[e]-dp[s-1])
+    a, b = map(int, input().split())
+    if a == 1:
+        print(prefix_sum[b - 1])
+    else:
+        print(prefix_sum[b - 1] - prefix_sum[a - 2])
